720fad45009ef476d5cbccf39ca85cb504fd310d
lab_services_student <St10496054@vcconnect.edu.za>
2025-06-19 12:44:38 +0200

    s

--- app/src/main/java/vcmsa/ci/mypracticumassignment/AddSong.kt
+++ app/src/main/java/vcmsa/ci/mypracticumassignment/AddSong.kt
@@ -1,6 +1,9 @@
 package vcmsa.ci.mypracticumassignment
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,6 +13,16 @@ class AddSong : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val submitButton = findViewById<Button>(R.id.button)
+        val titleEdit = findViewById<TextView>(R.id.textView)
+        val artistName = findViewById<EditText>(R.id.editTextText2)
+
+        submitButton.setOnClickListener {
+            val songTitle = titleEdit.toString()
+            val artistName = artistName.text.toString
+
+        }
         setContentView(R.layout.activity_add_song)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
